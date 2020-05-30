81b2b1158745a935cd818418ac3b93f5572b7624
TC <chengtian372@gmail.com>
2020-05-30 05:24:45 -0400

    login register

--- social_site/feed/views.py
+++ social_site/feed/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
-
-tweets = [{'name': 'Praveen','text':'This is my first tweet'},{'name': 'tc','text':'This is my second tweet'}]
+from .models import tweet
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import user_logged_out
 # Create your views here.
+
+@login_required
 def home(request):
-    context = {'tweets': tweets}
+    context = {'tweets': tweet.objects.all}
     return render(request,"feed/home.html",context)
--- social_site/social_site/urls.py
+++ social_site/social_site/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from accounts import views as acc_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('feed.urls')),
+    path('login/',auth_views.LoginView.as_view(template_name='accounts/login.html'), name = 'login'),
+    path('logout/',auth_views.LoginView.as_view(template_name='accounts/logout.html'), name = 'logout'),
+    path('register/', acc_views.register, name = 'register'),
 ]
