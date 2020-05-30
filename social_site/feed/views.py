f883eb24d8ecf47fad8265fec842a028a5715c26
TC <chengtian372@gmail.com>
2020-05-30 02:35:54 -0400

    base and homepage style demo

--- social_site/feed/views.py
+++ social_site/feed/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
+tweets = [{'name': 'Praveen','text':'This is my first tweet'},{'name': 'tc','text':'This is my second tweet'}]
 # Create your views here.
+def home(request):
+    context = {'tweets': tweets}
+    return render(request,"feed/home.html",context)
